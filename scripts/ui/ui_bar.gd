ec3447004173689f36c46cf65bc9b7c58b5eb7f2
Ghost9841 <asubedi778@gmail.com>
2025-10-22 18:47:47 +0545

    Added Health Bar Progress Bar, Scence Main Menu, Splash Screen, World, TileSet

---
+++ scripts/ui/ui_bar.gd
@@ -0,0 +1,12 @@
+extends Control
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $PlayerInfoBox/AnimatedSprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	animated_sprite_2d.play("Male_Player")
